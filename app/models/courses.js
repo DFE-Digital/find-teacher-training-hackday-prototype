7343bd19d88bd7acad357a14bf4fc4bb13a46fb6
Simon Whatley <simonwhatley@gmail.com>
2023-01-12 10:01:34 +0000

    Get course data

--- app/models/courses.js
+++ app/models/courses.js
@@ -2,6 +2,7 @@ const path = require('path')
 const fs = require('fs')
 
 const organisationModel = require('./organisations')
+const utils = require('../utils')()
 
 exports.findMany = (params) => {
   let courses = []
@@ -13,8 +14,6 @@ exports.findMany = (params) => {
     isAccreditedBody: true
   })
 
-  console.log(organisations.length);
-
   // put courses into memory
   organisations.forEach((organisation, i) => {
     const directoryPath = path.join(__dirname, '../data/courses/' + organisation.id)
@@ -25,46 +24,60 @@ exports.findMany = (params) => {
 
     documents.forEach((filename) => {
       const raw = fs.readFileSync(directoryPath + '/' + filename)
-      const data = JSON.parse(raw)
-      courses.push(data)
+      let course = JSON.parse(raw)
+
+      // only get courses that are published (open or closed), aka 'findable'
+      if ([1,4].includes(course.status)) {
+        course = utils.decorateCourse(course)
+
+        courses.push(course)
+      }
     })
   })
 
+  console.log('All:', courses.length);
 
-
-
-  // filter courses
-
-  // if (params.organisationId) {
-  //   const directoryPath = path.join(__dirname, '../data/courses/' + params.organisationId)
-  //
-  //   // to prevent errors when an organisation doesn't have any courses
-  //   // create an empty course directory for the organisation
-  //   if (!fs.existsSync(directoryPath)) {
-  //     fs.mkdirSync(directoryPath)
-  //   }
-  //
-  //   let documents = fs.readdirSync(directoryPath, 'utf8')
-  //
-  //   // Only get JSON documents
-  //   documents = documents.filter(doc => doc.match(/.*\.(json)/ig))
-  //
-  //   documents.forEach((filename) => {
-  //     const raw = fs.readFileSync(directoryPath + '/' + filename)
-  //     const data = JSON.parse(raw)
-  //     courses.push(data)
-  //   })
-  // }
-  //
   if (params.cycleId) {
     courses = courses.filter(course => course.cycle === params.cycleId)
   }
-  //
-  // if (params.courseCode) {
-  //   courses = courses.filter(course => course.code === params.courseCode)
+
+  console.log('2023:', courses.length);
+
+  if (params.subjects) {
+    courses = courses.filter(course => {
+      const subjectCodes = course.subjects.map(subject => {
+        return subject.code
+      })
+
+      return subjectCodes.filter(code => params.subjects.includes(code)).length !== 0
+    })
+  }
+
+  console.log('Subjects:', courses.length);
+
+  if (params.qualification) {
+    courses = courses.filter(course => params.qualification.includes(course.qualification))
+  }
+
+  console.log('Quals:', courses.length);
+
+  if (params.study_type) {
+    courses = courses.filter(course => params.study_type.includes(course.studyMode))
+  }
+
+  console.log('studyMode:', courses.length);
+
+  if (params.funding_type) {
+    courses = courses.filter(course => params.funding_type.includes(course.fundingType))
+  }
+
+  console.log('fundingType:', courses.length);
+
+  // if (typeof (params.has_vacancies) === 'boolean') {
+  //   courses = courses.filter(course => course.hasVacancies === params.has_vacancies)
   // }
 
-  console.log(courses.length);
+  // console.log('Vacancies:', courses.length);
 
   return courses
 }
