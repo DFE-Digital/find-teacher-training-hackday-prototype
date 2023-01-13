02961266ae37481ed0612893505c76449c64618a
Simon Whatley <simonwhatley@gmail.com>
2023-01-13 18:19:31 +0000

    Update course controller to use the new getCourse method

--- app/controllers/courses.js
+++ app/controllers/courses.js
@@ -1,49 +1,51 @@
 const qs = require('qs')
 const teacherTrainingService = require('../services/teacher-training')
-const utils = require('../utils')()
+// const utils = require('../utils')()
 
 exports.show = async (req, res) => {
   const providerCode = req.params.providerCode.toUpperCase()
   const courseCode = req.params.courseCode.toUpperCase()
 
   try {
-    const courseSingleResponse = await teacherTrainingService.getCourse(providerCode, courseCode)
-    const course = utils.decorateCourse(courseSingleResponse.data.attributes)
+    const course = await teacherTrainingService.getCourse(providerCode, courseCode)
+    const provider = await teacherTrainingService.getProvider(providerCode)
+    // const course = utils.decorateCourse(courseSingleResponse)
 
-    if (course.accredited_body_code) {
-      const providerSingleResponse = await teacherTrainingService.getProvider(course.accredited_body_code)
-      course.accredited_body = utils.decorateProvider(providerSingleResponse)
-    }
-
-    const locationListResponse = await teacherTrainingService.getCourseLocations(providerCode, courseCode)
-    const statuses = locationListResponse.included.filter(item => item.type === 'location_statuses')
-    const locations = locationListResponse.data.map(location => {
-      const { attributes } = location
-
-      // Vacancy status
-      const statusId = location.relationships.location_status.data.id
-      const status = statuses.find(status => status.id === statusId)
-      attributes.has_vacancies = status.attributes.has_vacancies
-
-      // Address
-      const streetAddress1 = attributes.street_address_1 ? attributes.street_address_1 + ', ' : ''
-      const streetAddress2 = attributes.street_address_2 ? attributes.street_address_2 + ', ' : ''
-      const city = attributes.city ? attributes.city + ', ' : ''
-      const county = attributes.county ? attributes.county + ', ' : ''
-      const postcode = attributes.postcode
-
-      attributes.name = attributes.name.replace(/'/g, '’')
-      attributes.address = `${streetAddress1}${streetAddress2}${city}${county}${postcode}`
-
-      return attributes
-    })
+    // if (course.accredited_body_code) {
+    //   const providerSingleResponse = await teacherTrainingService.getProvider(course.accredited_body_code)
+    //   course.accredited_body = utils.decorateProvider(providerSingleResponse)
+    // }
+    //
+    // const locationListResponse = await teacherTrainingService.getCourseLocations(providerCode, courseCode)
+    // const statuses = locationListResponse.included.filter(item => item.type === 'location_statuses')
+    // const locations = locationListResponse.data.map(location => {
+    //   const { attributes } = location
+    //
+    //   // Vacancy status
+    //   const statusId = location.relationships.location_status.data.id
+    //   const status = statuses.find(status => status.id === statusId)
+    //   attributes.has_vacancies = status.attributes.has_vacancies
+    //
+    //   // Address
+    //   const streetAddress1 = attributes.street_address_1 ? attributes.street_address_1 + ', ' : ''
+    //   const streetAddress2 = attributes.street_address_2 ? attributes.street_address_2 + ', ' : ''
+    //   const city = attributes.city ? attributes.city + ', ' : ''
+    //   const county = attributes.county ? attributes.county + ', ' : ''
+    //   const postcode = attributes.postcode
+    //
+    //   attributes.name = attributes.name.replace(/'/g, '’')
+    //   attributes.address = `${streetAddress1}${streetAddress2}${city}${county}${postcode}`
+    //
+    //   return attributes
+    // })
 
     // Mock course visa sponsorship
     // course.canSponsorVisa = (Math.random(course.code) > 0.5)
 
     // Mock placement schools
     // Assume placement schools are locations with a campus code that isn’t '-'
-    const schools = locations.filter(location => location.code !== '-')
+    // const schools = locations.filter(location => location.code !== '-')
+    const schools = []
 
     // Mock school placement policy
     // Assume provider has chosen to show placement schools if a course has more than 1 school location
@@ -73,6 +75,7 @@ exports.show = async (req, res) => {
     }
 
     res.render('courses/index', {
+      provider,
       course,
       schools,
       actions: {
